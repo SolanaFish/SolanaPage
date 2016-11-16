f0a30411ce50653f5f39da32972286274946799a
SolanaFish <michaql1@gmail.com>
2016-11-16 22:31:57 +0100

    implemented settings of reddit walpapers module with some additional features

--- modules/reddit-wallpapers-module/src/reddit.js
+++ modules/reddit-wallpapers-module/src/reddit.js
@@ -1,6 +1,14 @@
 const settingsDir = "./modules/reddit-wallpapers-module/settings.json";
-var fs = require('fs');
-var fetch = require('node-fetch');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+var readyUrls = [];
+const pug = require('pug');
+
+const uep = bodyParser.urlencoded({
+    extended: false
+});
+
 var settings = {
     load: () => {
         if (fs.existsSync(settingsDir)) {
@@ -8,17 +16,23 @@ var settings = {
         }
     },
     current: {
-        subreddits: ["EarthPorn"]
-    }
+        subreddits: ["EarthPorn"],
+        refresh: 120,
+        links: 50
+    },
+    save: () => {
+        serverLog('Saving reddit settings to local file');
+        var stringified = JSON.stringify(settings.current, null, 4);
+        fs.writeFileSync(settingsDir, stringified);
+    },
 };
-var readyUrls = [];
 
 function serverLog(text) {
     var date = new Date();
     console.log("[ " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " ] " + text);
 }
 
-function getRandomWallpaperURL() {
+function getRandomWallpaper() {
     return readyUrls[Math.floor(Math.random() * readyUrls.length)];
 }
 
@@ -50,13 +64,21 @@ function fetchURLs() {
 
 function getUrlsFromSubreddit(subreddit) {
     return new Promise((resolve, reject) => {
-        fetch(`https://reddit.com/r/${subreddit}.json`).then((res) => {
+        fetch(`https://reddit.com/r/${subreddit}.json?limit=${settings.links}`).then((res) => {
             return res.json();
         }).then((json) => {
             var urls = [];
             json.data.children.forEach((element) => {
-                var url = element.data.url.replace(/&amp;/g,"&"); // This is a hotfix for broken reddit api 
-                urls.push(url);
+                var url = element.data.url.replace(/&amp;/g, "&"); // This is a hotfix for broken reddit api
+                var permalink = element.data.permalink;
+                var subreddit = element.data.subreddit;
+                var title = element.data.title;
+                urls.push({
+                    url,
+                    permalink,
+                    subreddit,
+                    title
+                });
             });
             resolve(urls);
         }).catch((err) => {
@@ -66,21 +88,63 @@ function getUrlsFromSubreddit(subreddit) {
 }
 
 module.exports = (app) => {
-    app.get('/reddit-wallpapers-module/script.js', scriptJS);
+    settings.load();
+
     updateUrls();
-    setInterval(updateUrls, 5 * 60 * 1000);
-    app.get('/randomWallpaper', (req, res) => {
-        res.send(getRandomWallpaperURL());
-    });
+    setInterval(updateUrls, settings.current.refresh * 60 * 1000);
+
+    app.get('/randomWallpaper', randomWall);
+    app.get('/reddit-wallpapers-module/script.js', scriptJS);
+    app.post('/redditWallpaper/setRefresh', uep, setRefresh);
+    app.post('/redditWallpaper/setSubreddits', uep, setSubreddits);
+    app.post('/redditWallpaper/setLinks',uep, setLinks);
+
     serverLog('Reddit wallpaper module ready!');
+};
+
+var randomWall = (req, res) => {
+    var wall = getRandomWallpaper();
+    var info = pug.renderFile(`${__dirname}/../views/info.pug`, {
+        wall: wall
+    });
+    res.send({
+        wallUrl: wall.url,
+        info: info
+    });
 };
 
 var scriptJS = (req, res) => {
     res.sendFile(`${__dirname}/script.js`);
 };
 
+var setRefresh = (req, res) => {
+    const refresh = req.body.refresh;
+    if (refresh != settings.current.refresh) {
+        settings.current.refresh = refresh;
+        settings.save();
+    }
+    res.sendStatus(200);
+};
+
+var setLinks = (req, res) => {
+    const links = req.body.links;
+    if (links != settings.current.links) {
+        settings.current.links = links;
+        settings.save();
+    }
+    res.sendStatus(200);
+};
+
+var setSubreddits = (req, res) => {
+    settings.current.subreddits = JSON.parse(req.body.subs);
+    settings.save();
+    res.sendStatus(200);
+};
+
 module.exports.getSettings = () => {
-    return null;
+    return Promise.resolve(pug.renderFile(`${__dirname}/../views/settings.pug`, {
+        settings: settings.current
+    }));
 };
 
 module.exports.getMainView = () => {
--- modules/reddit-wallpapers-module/src/script.js
+++ modules/reddit-wallpapers-module/src/script.js
@@ -1,8 +1,85 @@
-function redditWallpaper () {
+function redditWallpaper() {
     fetch('/randomWallpaper').then(function(res) {
         return res.text();
     }).then(function(res) {
-        document.body.style.backgroundImage=`url(${res})`;
+        if (res) {
+            var wallData = JSON.parse(res);
+            document.body.style.backgroundImage = `url(${wallData.wallUrl})`;
+
+            document.getElementById('wallpaperInfo').innerHTML = wallData.info;
+        }
     }).catch(console.error);
 }
+
+function submitRefresh() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/redditWallpaper/setRefresh", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("refresh=" + document.getElementById('redditRefreshSlider').value);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const toast = document.getElementById('refreshToast');
+            toast.open();
+        }
+    };
+}
+
+function submitLinks() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/redditWallpaper/setLinks", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("links=" + document.getElementById('redditLinksSlider').value);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const toast = document.getElementById('linksToast');
+            toast.open();
+        }
+    };
+}
+
+function submitSubreddits() {
+    var redditInputs = document.getElementById('redditInputs');
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/redditWallpaper/setSubreddits", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var message = [];
+    Array.prototype.slice.call(redditInputs.children).forEach((child) => {
+        if (child.value) {
+            message.push(child.value);
+            console.log(message.toString());
+        }
+    });
+    xhttp.send("subs=" + JSON.stringify(message));
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const toast = document.getElementById('subredditsToast');
+            toast.open();
+        }
+    };
+}
+
+
+function setupSlider() {
+    var refreshSlider = document.getElementById('redditRefreshSlider');
+    refreshSlider.addEventListener('change', () => {
+        submitRefresh();
+    });
+    var linksSlider = document.getElementById('redditLinksSlider');
+    linksSlider.addEventListener('change', () => {
+        submitLinks();
+    });
+    var redditInputs = document.getElementById('redditInputs');
+    var lastChildListen = () => {
+        if (redditInputs.lastChild.value !== "") {
+            redditInputs.lastChild.removeEventListener('change', lastChildListen);
+            redditInputs.appendChild(redditInputs.lastChild.cloneNode(true));
+            //last child changes
+            redditInputs.lastChild.value = "";
+            redditInputs.lastChild.addEventListener('change', lastChildListen);
+            redditInputs.scrollIntoView();
+        }
+    };
+    redditInputs.lastChild.addEventListener('change', lastChildListen);
+}
+
 redditWallpaper();
