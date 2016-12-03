f33c2c562bd593800d8623cb0f61c80f75212ae4
SolanaFish <michaql1@gmail.com>
2016-12-03 23:29:09 +0100

    Added new test's for reddit module

--- tests/reddit-module-test.js
+++ tests/reddit-module-test.js
@@ -1,32 +1,81 @@
 const rewire = require('rewire');
+const express = require('express');
 const chai = require('chai');
 const should = chai.should();
-const express = require('express');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
 describe('reddit-wallpapers-module', function() {
     var redditModule;
     var app;
-    before(function() {
+    before(function(done) {
         app = express();
         redditModule = rewire('../modules/reddit-wallpapers-module/src/reddit.js');
-        redditModule(app);
+        redditModule(app).then(() => {
+            done();
+        });
     });
-    beforeEach(function() {});
     it('Should have all settings loaded', () => {
         var settings = redditModule.__get__('settings');
-        settings.current.should.have.property('subreddits');
-        settings.current.should.have.property('refresh');
-        settings.current.should.have.property('links');
+        settings.current.should.have.property('subreddits').and.not.be.empty;
+        settings.current.should.have.property('refresh').and.not.be.empty;
+        settings.current.should.have.property('links').and.not.be.empty;
+        settings.current.should.have.property('checkUrls');
     });
     it('Should have meaningful values in current settings', () => {
         var settings = redditModule.__get__('settings');
         settings.current.refresh.should.be.above(0);
         settings.current.links.should.be.above(0);
     });
-    it('Should be able to send new random wallpaper', () => {
+    it('Should be able to return new random wallpaper', () => {
         var getRandomWallpaper = redditModule.__get__('getRandomWallpaper');
         var wallUrl = getRandomWallpaper();
-        console.log(wallUrl);
-        wallUrl.should.not.be.empty();
+        wallUrl.should.have.property('url').and.not.be.empty;
+        wallUrl.should.have.property('permalink').and.not.be.empty;
+        wallUrl.should.have.property('subreddit').and.not.be.empty;
+        wallUrl.should.have.property('title').and.not.be.empty;
+    });
+    it('Should be able to fetch wallpapers from single subreddit', () => {
+        var getUrlsFromSubreddit = redditModule.__get__('getUrlsFromSubreddit')('EarthPorn');
+        getUrlsFromSubreddit.then((urls) => {
+            urls.should.have.length.above(0);
+            urls.forEach((url) => {
+                wallUrl.should.have.property('url').and.not.be.empty;
+                wallUrl.should.have.property('permalink').and.not.be.empty;
+                wallUrl.should.have.property('subreddit').and.not.be.empty;
+                wallUrl.should.have.property('title').and.not.be.empty;
+            });
+        });
+    });
+    it('Should be able to fetch wallpaper from multiple subreddits', () => {
+        redditModule.__with__({
+            settings: {
+                current: {
+                    subreddits: ['EarthPorn', 'VillagePorn'],
+                    refresh: 120,
+                    links: 50,
+                    checkUrls: false
+                }
+            }
+        })(() => {
+            var fetchURLs = redditModule.__get__('fetchURLs')();
+            fetchURLs.then((urls) => {
+                urls.forEach((url) => {
+                    wallUrl.should.have.property('url').and.not.be.empty;
+                    wallUrl.should.have.property('permalink').and.not.be.empty;
+                    wallUrl.should.have.property('subreddit').and.not.be.empty;
+                    wallUrl.should.have.property('title').and.not.be.empty;
+                });
+            });
+        });
+    });
+    it('Should be able to send new wallpaper', (done) => {
+        chai.request(app)
+            .get('/randomWallpaper')
+            .end((err, res) => {
+                res.should.have.status(200);
+                
+                done();
+            });
     });
 });
